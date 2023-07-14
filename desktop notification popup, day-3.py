d3bd64dcccab2d38d5800905c901eedd328f24c9
Ronit Roy <92022523+Ronit027@users.noreply.github.com>
2023-07-14 07:58:03 +0530

    Add files via upload

---
+++ desktop notification popup, day-3.py
@@ -0,0 +1,8 @@
+import time
+from winotify import Notification, audio
+toast=Notification(app_id="Ronit Roy",
+                   title="Message Title",
+                   msg="Hello! This is Ronit",
+                   duration="short",
+                   icon=r"C:\Users\hp\Downloads\Picsart_22-12-01_12-52-08-925.jpg")
+toast.show()
